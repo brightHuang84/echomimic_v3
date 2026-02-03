6aa3227abeb9d3c1e9a7674cc30125e75ae3e174
bright <bright@bright-hannah.com>
2026-02-03 10:14:12 +0700

    fix bug

--- infer_flash_pro.py
+++ infer_flash_pro.py
@@ -212,6 +212,7 @@ def main():
     wav2vec_feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(wav2vec_model_dir, local_files_only=True)
 
     device = set_multi_gpus_devices(ulysses_degree, ring_degree)
+    device = torch.device(device) if isinstance(device, str) else device
     if device.type == "cpu" and torch.cuda.is_available():
         device = torch.device("cuda", 0)
         print(">>> [DEBUG] overridden device to", device)
